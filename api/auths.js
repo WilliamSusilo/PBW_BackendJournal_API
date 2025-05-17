32b23386dd1229c5f30ad0806621c0a2ced413d3
William <williamsusilo810@gmail.com>
2025-05-17 14:46:46 +0700

    Add allow CORS policy

--- api/auths.js
+++ api/auths.js
@@ -20,6 +20,14 @@ module.exports = async (req, res) => {
   const body = req.body;
   const { action } = body;
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     switch (action) {
       // Register Endpoint
